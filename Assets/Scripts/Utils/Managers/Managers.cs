98a91d0de2f9a7c6100309c08764fa22ea602c11
Donghee <luz0415@naver.com>
2024-05-30 15:58:27 +0900

    🔨 Add | TableManager 추가

--- Assets/Scripts/Utils/Managers/Managers.cs
+++ Assets/Scripts/Utils/Managers/Managers.cs
@@ -7,9 +7,11 @@ public class Managers : MonoSingleton<Managers>
 {
     private ResourceManager _resources = new ResourceManager();
     private UIManager _ui = new UIManager();
+    private TableManager _data = new TableManager();
 
     public Action GameOverAction = null;
 
     public static ResourceManager Resources { get { return Instance._resources; } } 
     public static UIManager UI { get { return Instance._ui; } }
+    public static TableManager Table { get { return Instance._data; } }
 }
