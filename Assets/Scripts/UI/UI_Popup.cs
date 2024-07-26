1ca75077e92ba288ad4b0367bb7e4ba13b6fa178
강승연 <syeon.dev@gmail.com>
2024-07-26 16:19:08 +0900

    🐛Fix | 인벤토리 팝업 리팩토링 후 작동하지 않던 Slot 기능 해결

--- Assets/Scripts/UI/UI_Popup.cs
+++ Assets/Scripts/UI/UI_Popup.cs
@@ -7,7 +7,7 @@ public class UI_Popup : UI_Base
 {
     public override void Init()
     {
-        Managers.UI.SetCanvas(gameObject, true);
+        //Managers.UI.SetCanvas(gameObject, true);
     }
 
     public virtual void ClosePopupUI(){
