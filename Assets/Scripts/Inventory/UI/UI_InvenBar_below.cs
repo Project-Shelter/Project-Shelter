41aa2ae119c97153f54d8c3d7b98b8f0f0a15a2b
Donghee <luz0415@naver.com>
2024-07-30 00:24:27 +0900

    🐛Fix | 하단바 ChangeSlot 꺼질때도 실행되던 문제 해결

--- Assets/Scripts/Inventory/UI/UI_InvenBar_below.cs
+++ Assets/Scripts/Inventory/UI/UI_InvenBar_below.cs
@@ -16,7 +16,7 @@ public class UI_InvenBar_below : UI_Container
         for (int i = 0; i < 6; i++)
         {
             int index = i;
-            slots[index].slotBtn.onValueChanged.AddListener((on)=> ChangeSlot(index));
+            slots[index].slotBtn.onValueChanged.AddListener((on)=> ChangeSlot(on, index));
         }
         slots[selectedSlot].slotBtn.isOn = true;
 
@@ -27,9 +27,9 @@ public class UI_InvenBar_below : UI_Container
         };
     }
 
-    private void ChangeSlot(int slotNumber)
+    private void ChangeSlot(bool isOn, int slotNumber)
     {
-        if(selectedSlot == slotNumber) return;
+        if (!isOn) return;
         selectedSlot = slotNumber;
         ServiceLocator.GetService<ActorController>().CurrentActor.SetItem(GetSelectedItem());
     }
@@ -37,44 +37,51 @@ public class UI_InvenBar_below : UI_Container
     //좌우 select 이동
     public void OnRight()
     {
-        Debug.Log("RIght");
         if (selectedSlot + 1 >= 6) return;
+        if(slots[selectedSlot+1].Item.id == 0) return;
         slots[++selectedSlot].slotBtn.isOn = true;
     }
     public void OnLeft()
     {
         if (selectedSlot - 1 < 0) return;
+        if (slots[selectedSlot - 1].Item.id == 0) return;
         slots[--selectedSlot].slotBtn.isOn = true;
     }
 
     public void OnFirst()
     {
-        
+        if (slots[0].Item.id == 0) return;
+        slots[0].slotBtn.isOn = true;
     }
 
     public void OnSecond()
     {
-        
+        if (slots[1].Item.id == 0) return;
+        slots[1].slotBtn.isOn = true;
     }
 
     public void OnThird()
     {
-        
+        if (slots[2].Item.id == 0) return;
+        slots[2].slotBtn.isOn = true;
     }
 
     public void OnFourth()
     {
-        
+        if (slots[3].Item.id == 0) return;
+        slots[3].slotBtn.isOn = true;
     }
 
     public void OnFifth()
     {
-        Debug.Log("Fifth");
+        if (slots[4].Item.id == 0) return;
+        slots[4].slotBtn.isOn = true;
     }
 
     public void OnSixth()
     {
-        
+        if (slots[5].Item.id == 0) return;
+        slots[5].slotBtn.isOn = true;
     }
 
     // 선택한 아이템 정보 가져오기
